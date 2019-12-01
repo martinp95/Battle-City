fe8fd1f5c5b1b31b2a6d0080f3960d14a6d05d44
martinp95 <martinpelaezdiaz@gmail.com>
2019-12-01 20:41:42 +0100

    Mostar jugador por encima de enemigo

--- src/layers/GameLayer.js
+++ src/layers/GameLayer.js
@@ -708,10 +708,11 @@ class GameLayer extends Layer {
         for (var i = 0; i < this.disparosEnemigo.length; i++) {
             this.disparosEnemigo[i].dibujar(this.scrollX, this.scrollY);
         }
-        this.jugador.dibujar(this.scrollX, this.scrollY);
         for (var i = 0; i < this.enemigos.length; i++) {
             this.enemigos[i].dibujar(this.scrollX, this.scrollY);
         }
+        this.jugador.dibujar(this.scrollX, this.scrollY);
+
         for(var i = 0; i < this.tuneles.length; i++){
             this.tuneles[i].dibujar(this.scrollX, this.scrollY);
         }
